4d3386272d83733918d411b51a2afaae1bc1875b
gyj-bf <sayatempemendoan@gmail.com>
2026-06-11 21:14:48 +0700

    build(docker): set timezone

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 docker stop 9router
 docker rm 9router
 docker build -t 9router .
-docker run -d --name 9router -p 20128:20128 --env-file .env -v "$HOME/.9router-dev:/app/data" -e DATA_DIR=/app/data 9router
+docker run -d --name 9router \
+  -p 20128:20128 \
+  --env-file .env \
+  -v "$HOME/.9router-dev:/app/data" \
+  -e DATA_DIR=/app/data \
+  9router
